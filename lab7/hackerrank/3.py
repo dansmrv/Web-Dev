18b1486df7a37dc64bc2a08efbf2effa749d2266
dansmrv <hurmaramen@gmail.com>
2023-03-26 01:00:11 +0600

    hackerrank

---
+++ lab7/hackerrank/3.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+
+sum=a+b
+diff = a - b
+prod = a * b
+print(sum, diff, prod, sep="\n")
---
+++ lab7/hackerrank/4.py
@@ -0,0 +1,4 @@
+import math 
+n = int(input())
+for x in range(n):
+  print(x**2)
